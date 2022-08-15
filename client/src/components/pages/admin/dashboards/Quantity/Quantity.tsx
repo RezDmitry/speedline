0ce61e37f7374ac67b4726f64293aad499b65e69
RezDmitry <dm.rezcov@it-justice.com>
2022-08-15 14:19:11 +0300

    Charts - feat - added time cases for diagram

--- client/src/components/pages/admin/dashboards/Quantity/Quantity.tsx
+++ client/src/components/pages/admin/dashboards/Quantity/Quantity.tsx
@@ -1,61 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactECharts from 'echarts-for-react';
-import { format } from 'date-fns';
 
 import { useAppDispatch, useAppSelector } from '../../../../../hooks/useStore';
 import { fetchProducts } from '../../../../../store/slices/actionCreators/product';
+import format from '../../../../../helpers/format';
+import {
+  checkDate, lineChartOption, setFormat, setIntervalByButton,
+} from './chartOptions';
 
 import styles from './Quantity.module.scss';
 
 const Quantity = () => {
   const dispatch = useAppDispatch();
   const { products } = useAppSelector((state) => state.productReducer);
-  const setWeekArray = (): Date [] => {
-    const today = new Date(Date.now());
-    today.setHours(0, 0, 0, 0);
-    const lastWeek = [];
-    for (let i = 0; i < 6; i++) {
-      const day = new Date(today.setHours(0, 0, 0, 0) - i * 86400000);
-      lastWeek.push(day);
-    }
-    return lastWeek.reverse();
-  };
-  const option = {
-    title: {
-      text: 'Products quantity',
-      x: 'center',
-      textStyle: {
-        fontFamily: 'Inter',
-        color: '#3E4C59',
-      },
+  const [timeInterval, setTimeInterval] = useState('week');
+  const option = { ...lineChartOption };
+  option.dataset = [
+    {
+      source: [
+        ['Date', 'Products'],
+        ...setIntervalByButton(timeInterval).map((date) => [
+          format(date, setFormat(timeInterval)),
+          products.filter((product) => checkDate(date, product, timeInterval)).length,
+        ]),
+      ],
     },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross',
-      },
-    },
-    xAxis: {
-      type: 'category',
-      data: setWeekArray().map((day) => format(day, 'dd/MM/yyyy')),
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        type: 'line',
-        data: setWeekArray().map((day: Date) => products
-          .filter((product) => new Date(Date.parse(product.createdAt)).getDate() === day.getDate()).length),
-      },
-    ],
-  };
+  ];
   useEffect(() => {
     dispatch(fetchProducts({}));
-    setWeekArray();
   }, []);
   return (
     <div className={styles.dimensions}>
+      <div className={styles.control}>
+        {['week', 'month', 'year'].map((str) => (
+          <button
+            onClick={() => setTimeInterval(str)}
+            className={timeInterval === str ? styles.active : ''}
+            key={str}
+          >
+            {str}
+          </button>
+        ))}
+      </div>
       <ReactECharts
         key={option.title.text}
         option={option}
--- client/src/components/pages/admin/dashboards/Workload/Workload.tsx
+++ client/src/components/pages/admin/dashboards/Workload/Workload.tsx
@@ -3,55 +3,27 @@ import ReactECharts from 'echarts-for-react';
 
 import { useAppDispatch, useAppSelector } from '../../../../../hooks/useStore';
 import { fetchWarehouses } from '../../../../../store/slices/actionCreators/warehouse';
+import { pieOption } from './chartOptions';
 
 import styles from './Workload.module.scss';
 
 const Workload = () => {
   const dispatch = useAppDispatch();
   const { warehouses } = useAppSelector((state) => state.warehouseReducer);
-  const option = {
-    title: {
-      text: 'Workload',
-      x: 'center',
-      textStyle: {
-        fontFamily: 'Inter',
-        color: '#3E4C59',
+  const option = { ...pieOption };
+  option.dataset = [
+    {
+      source: [
+        ['Warehouse', 'Quantity'], ...warehouses.map((warehouse) => [warehouse.name, warehouse.products.length]),
+      ],
+    },
+    {
+      transform: {
+        type: 'filter',
+        config: { dimension: 'Quantity', '!=': 0 },
       },
     },
-    tooltip: {
-      trigger: 'item',
-      formatter: '{a} <br/>{b} : {c} ({d}%)',
-    },
-    dataset: [
-      {
-        source: [
-          ['Warehouse', 'Quantity'], ...warehouses.map((warehouse) => [warehouse.name, warehouse.products.length]),
-        ],
-      },
-      {
-        transform: {
-          type: 'filter',
-          config: { dimension: 'Quantity', '!=': 0 },
-        },
-      },
-    ],
-    series: [
-      {
-        name: 'Warehouses workload',
-        type: 'pie',
-        datasetIndex: 1,
-        radius: '50%',
-        center: ['50%', '50%'],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)',
-          },
-        },
-      },
-    ],
-  };
+  ];
   useEffect(() => {
     dispatch(fetchWarehouses({}));
   }, []);
